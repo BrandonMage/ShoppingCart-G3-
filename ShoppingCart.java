3ab1e492d2ed3eebaef450a8fc40d6f89d4c5ceb
AbelSomili <abelsomili4@gmail.com>
2023-04-02 11:32:32 -1200

    HashMap and Scanner initialization

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -2,6 +2,10 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class ShoppingCart {
+    private static HashMap<Integer, String> products = new HashMap<Integer, String>();
+    private static HashMap<Integer, Integer> cart = new HashMap<Integer, Integer>();
+    private static Scanner scanner = new Scanner(System.in);
+
       public static void main(String[] args) {
 
   } 
