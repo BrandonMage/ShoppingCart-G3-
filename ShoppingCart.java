7be9ed4f9245f606eee4be32b638e566f1348af2
BrandonMage <128803583+BrandonMage@users.noreply.github.com>
2023-04-02 11:40:07 +0200

    Dummy products list
    Added 3 products to the products HashMap.
    Product 1 - Product 3

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -2,11 +2,16 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class ShoppingCart {
+  
     private static HashMap<Integer, String> products = new HashMap<Integer, String>();
     private static HashMap<Integer, Integer> cart = new HashMap<Integer, Integer>();
     private static Scanner scanner = new Scanner(System.in);
 
       public static void main(String[] args) {
+        // Adding products to the products HashMap
+        products.put(1, "Product 1");
+        products.put(2, "Product 1");
+        products.put(3, "Product 3");
 
   } 
 }
